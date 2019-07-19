889c56aa6bba96b0dc8d0649c3a19d9511bd62fc
adileone <dileone.ale@gmail.com>
2019-07-19 11:04:19 +0200

    basics add

--- go_basics/main.go
+++ go_basics/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 /*
 Go ha solo un costrutto ciclico, il ciclo for.
@@ -17,11 +21,39 @@ Il for ha tre componenti separati da punto e virgola:
 Nota: diversamente da altri linguaggi come C, Java, o Javascript i tre componenti del costrutto ciclico non sono tra parentesi ( ) e le parentesi { } sono obbligatorie.
 */
 
-func main() {
-
+func for_cycle() string {
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
 	}
-	fmt.Println(sum)
+	return strconv.Itoa(sum)
+
+}
+
+//Ricettori puntatori
+
+/*
+Metodi con ricettori puntatori possono modificare il valore al quale puntano.
+Con un ricettore di tipo valore, il metodo Scale opera su una copia del valore originale Vertex.
+*/
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+//Vertex con e senza puntatore comportamenti differenti
+func (v Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	fmt.Println(for_cycle())
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
